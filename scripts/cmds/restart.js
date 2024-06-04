a5972a39ddad50d2dc0ce888aaa816dde2df3ce6
Empereursaid1 <julbertinezoungrana@gmail.com>
2024-06-04 18:44:39 +0000

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -23,7 +23,7 @@ module.exports = {
 			restartting: "🔄 | Đang khởi động lại bot..."
 		},
 		en: {
-			restartting: "🔄 | Restarting bot..."
+			restartting: "🔄 | 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐈𝐍 ಠ⁠_⁠ಠ 𝐁𝐎𝐓..."
 		}
 	},
 
@@ -31,7 +31,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | 𝗕𝗢𝗧 𝗥𝗘𝗦𝗧𝗔𝗥𝗧𝗥𝗗 \n🟢 | Time: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
