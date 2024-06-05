dc91b6622366ef63dbe3aa11068e7947f01f9325
Empereursaid1 <julbertinezoungrana@gmail.com>
2024-06-05 17:20:14 +0000

    Update Salut.js

--- scripts/cmds/Salut.js
+++ scripts/cmds/Salut.js
@@ -5,8 +5,8 @@ module.exports = {
         author: "ᏋᎷᎮᏋᏒᏋᏬᏒ ᏕᎤᎥᎴ💦",
         countDown: 5,
         role: 0,
-        shortDescription: "sarcasm",
-        longDescription: "sarcasm",
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
         category: "reply",
     },
 onStart: async function(){}, 
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "salut") return message.reply(".𖤍𝐒𝐀𝐋𝐔𝐓𖤍.  𝐣𝐞 𝐦'𝐚𝐩𝐩𝐞𝐥𝐥𝐞 -𝐏𝐀𝐁𝐋𝐎- 𝐣𝐞 𝐬𝐮𝐢𝐬 𝐥𝐚 𝐩𝐨𝐮𝐫 𝐫𝐞𝐩𝐨𝐧𝐝𝐫𝐞 𝐚 𝐭𝐨𝐮𝐭 𝐯𝐨𝐬 𝐪𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 ☞︎︎︎𖤍☜︎︎︎");
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("𖤍𝐒𝐀𝐋𝐔𝐓𖤍  𝐦𝐨𝐢 𝐜'𝐞𝐬𝐭 -𝐏𝐀𝐁𝐋𝐎- 𝐣𝐞 𝐬𝐮𝐢𝐬 𝐥𝐚 𝐩𝐨𝐮𝐫 𝐫𝐞𝐩𝐨𝐧𝐝𝐫𝐞 𝐚 𝐭𝐨𝐮𝐭 𝐯𝐨𝐬 𝐪𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 ☞︎︎︎𖤍☜︎︎︎");
 }
 };
