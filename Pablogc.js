f54206e6fb7fe834d8e157e56a1ea70266372f6a
Empereursaid1 <julbertinezoungrana@gmail.com>
2024-06-07 15:57:39 +0000

    Update and rename scripts/cmds/ghost.js to Pablogc.js

--- Pablogc.js
+++ Pablogc.js
@@ -31,7 +31,7 @@ module.exports = {
       const participants = threadInfo.participantIDs;
 
       if (participants.includes(event.senderID)) {
-        api.sendMessage("『✍︎ 🥷𝘛'𝘦𝘴 𝘥𝘦𝘫𝘢 𝘥𝘢𝘯𝘴 𝘭𝘦 𝘨𝘳𝘰𝘶𝘱𝘦 𝘴𝘪 𝘵𝘶 𝘵𝘳𝘰𝘶𝘷𝘦𝘴 𝘱𝘢𝘴 𝘷𝘦𝘳𝘪𝘧𝘪𝘦 𝘵𝘢 𝘣𝘰𝘪𝘵𝘦 𝘥𝘦 𝘮𝘦𝘴𝘴𝘢𝘨𝘦𝘴 𝘦𝘵 𝘴𝘱𝘢𝘮 ✍︎』", event.threadID);
+        api.sendMessage("『🥷𝘛'𝘦𝘴 𝘥𝘦𝘫𝘢 𝘥𝘢𝘯𝘴 𝘭𝘦 𝘨𝘳𝘰𝘶𝘱𝘦 𝘴𝘪 𝘵𝘶 𝘵𝘳𝘰𝘶𝘷𝘦𝘴 𝘱𝘢𝘴 𝘷𝘦𝘳𝘪𝘧𝘪𝘦 𝘵𝘢 𝘣𝘰𝘪𝘵𝘦 𝘥𝘦 𝘮𝘦𝘴𝘴𝘢𝘨𝘦𝘴 𝘦𝘵 𝘴𝘱𝘢𝘮』", event.threadID);
 
         // Set ⚠ reaction for already added user
         api.setMessageReaction("⚠", event.messageID, "🥷", api);
@@ -40,13 +40,13 @@ module.exports = {
         await api.addUserToGroup(event.senderID, threadID);
         api.sendMessage("🎊 | 𝑻'𝒂𝒔 𝒆𝒕𝒆 𝒂𝒋𝒐𝒖𝒕𝒆 𝒂𝒖 𝒈𝒓𝒐𝒖𝒑𝒆 🥷🂱➪ 𝐏𝐀𝐁𝐋𝐎 𖤍 𝐆𝐂....🂱🥷☞︎︎︎𖤍𝐁𝐎𝐓𖤍☜︎︎︎", event.threadID);
 
-        // Set 💛 reaction for successfully added user
+        // Set 👌🏽 reaction for successfully added user
         api.setMessageReaction("✨", event.messageID, "🥷", api);
       }
     } catch (error) {
-      api.sendMessage("🙎🏽 | ❌Non une erreur s'est produite ❌   🥷réessaye 🥷", event.threadID);
+      api.sendMessage("🙅🏾 |❌échec de l'ajout❌ 🥷réessaye pour voir 🥷", event.threadID);
 
-      // Set 🙆 reaction for failed adding user
+      // Set 🙍🏾 reaction for failed adding user
       api.setMessageReaction("💀", event.messageID, "👍", api);
     }
   }
