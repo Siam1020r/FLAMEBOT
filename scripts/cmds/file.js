a4afbebeffc901487d77324537ca7ffcd3f1b451
Siam1020r <siam1020r@gmail.com>
2024-06-16 00:20:32 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61550814914251"];
+    const permission = ["100044530383890"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("༺🖕(°ㅂ° ╬)🖕|𝐓𝐮 𝐧'𝐞𝐬 𝐩𝐚𝐬⚜️ᏋᎷᎮᏋᏒᏋᏬᏒ ᏕᎤᎥᎴ⚜️𝐝𝐞𝐠𝐚𝐠𝐞😷𝐜𝐨𝐧𝐧𝐚𝐫𝐝𝐬༻  ", event.threadID, event.messageID);
+      return api.sendMessage("only MD ABDUR RAHMAN SIAM use this commanf", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("𝐕𝐨𝐮𝐬 𝐚𝐯𝐞𝐳 𝐛𝐞𝐬𝐨𝐢𝐧𝐬 𝐝𝐞 𝐪𝐮𝐞𝐥 𝐟𝐢𝐜𝐡𝐢𝐞𝐫?༺𝐁𝐎𝐒𝐒༻? ಠωಠ.", event.threadID, event.messageID);
+      return api.sendMessage("file not found ", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`✍︎𝐉'𝐚𝐢 𝐩𝐚𝐬 𝐜̧𝐚 [⚠︎]: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage([⚠︎]: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
